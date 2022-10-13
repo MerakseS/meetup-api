5b543a3134dd153ad0ec7e43a1809ea1777cfb15
MerakseS <lsnwot@gmail.com>
2022-10-13 13:47:30 +0300

    #0 Initial commit

---
+++ src/main/java/by/losevsa/eventscrud/EventscrudApplication.java
@@ -0,0 +1,13 @@
+package by.losevsa.eventscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventscrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventscrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/losevsa/eventscrud/EventscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package by.losevsa.eventscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventscrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
